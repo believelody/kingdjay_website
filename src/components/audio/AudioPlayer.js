1c7376470a12cf63a1bed01c76d7e8469d132df9
believelody <believelody@gmail.com>
2018-10-08 01:11:21 +0200

    move player features from mixes component to audioplayer component

--- src/components/audio/AudioPlayer.js
+++ src/components/audio/AudioPlayer.js
@@ -15,27 +15,130 @@ class AudioPlayer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loop: ''
+      loop: '',
+      playing: false,
+      paused: false,
+      currentTime: 0,
+      duration: 0
     };
   }
 
+  componentDidMount() {
+    this.setState({playing: true, paused: false, currentTime: 0}, this.playAudio);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.current !== -1) {
+      this.setState({playing: true, paused: false, currentTime: 0}, this.playAudio);
+    }
+  }
+
+  timer = () => this.setState({currentTime: this.audioElement.currentTime});
+
   ShuffleRepeat = value => this.setState({ loop: value });
 
+  loadDuration = () => {
+    // console.log(this.audioElement.duration);
+    this.setState({duration: this.audioElement.duration});
+  }
+
+  playAudio = () => {
+    console.log(this.props.current);
+    if (this.props.current !== undefined) {
+      if (this.state.currentTime > 0) {
+        this.audioElement.currentTime = this.state.currentTime;
+      }
+      else {
+        this.audioElement.load();
+      }
+
+      this.audioElement.play();
+    }
+  }
+
+  pauseAudio = () => {
+    // clearInterval(this.state.interval);
+    this.setState({ currentTime: this.audioElement.currentTime });
+    this.audioElement.pause();
+  }
+
+  stopAudio = () => {
+    // clearInterval(this.state.interval);
+    this.audioElement.currentTime = 0;
+    this.audioElement.pause();
+  }
+
+  handleSeek = value => {
+    // console.log(value);
+    this.audioElement.currentTime = value;
+  }
+
+  handleClick = (e, {id}, value = 0) => {
+    switch (id) {
+      case 'play':
+        this.setState({playing:true, paused: false}, this.playAudio);
+        break;
+      case 'pause':
+        this.setState({playing: false, paused: true}, this.pauseAudio);
+        break;
+      case 'stop':
+        this.setState({playing: false, paused: false, currentTime: 0}, this.stopAudio);
+        break;
+      case "prev":
+        this.setState((state, props) => {
+          let currentIndex = props.currentTrackIndex - 1;
+          if (currentIndex < 0) {
+            return {currentTime : 0};
+          } else {
+            this.props.changeIndex(currentIndex);
+            return { playing:true, paused: false, currentTime: 0 };
+          }
+        },this.playAudio);
+        break;
+      case "next":
+        this.setState((state, props) => {
+          let currentIndex = props.currentTrackIndex + 1;
+          if (currentIndex > this.props.length - 1) {
+            return {currentTime : 0};
+          } else {
+            this.props.changeIndex(currentIndex);
+            return { playing:true, paused: false, currentTime: 0 };
+          }
+        },this.playAudio);
+        break;
+      default:
+        break;
+    }
+  }
+
   render() {
-    const { loop } = this.state;
-    const { current, handleClick, handleSeek, playing, currentTime, duration } = this.props;
+    const { loop, currentTime, duration, playing, paused } = this.state;
+    const { current } = this.props;
     return (
       <Segment className='player'>
-        {currentTime > 0 && <ProgressBar handleSeek={handleSeek} currentTime={currentTime} duration={duration} />}
+        <ProgressBar
+          handleSeek={this.handleSeek}
+          currentTime={currentTime}
+          duration={duration}
+        />
         <Button.Group icon>
-          <Backward handlePrev={handleClick} />
-          <PlayPause playing={playing} handlePlayPause={handleClick} />
-          <Stop handleStop={handleClick}p />
-          <Forward handleNext={handleClick} />
+          <Backward handlePrev={this.handleClick} />
+          <PlayPause playing={playing} handlePlayPause={this.handleClick} />
+          <Stop handleStop={this.handleClick}p />
+          <Forward handleNext={this.handleClick} />
           <ShuffleRepeat handleClick={this.ShuffleRepeat} value={loop} />
         </Button.Group>
-        <Content artist={current.artist} title={current.title} />
+        <Content artist={current ? current.artist : ''} title={current ? current.title : ''} />
         <Duration currentTime={currentTime} duration={duration} />
+        {
+          current &&
+          <audio
+            ref={audio => {this.audioElement = audio}}
+            src={current.src}
+            onLoadedMetadata={this.loadDuration}
+            onTimeUpdate={this.timer}
+          />
+        }
       </Segment>
     );
   }
--- src/components/mix/Mixes.js
+++ src/components/mix/Mixes.js
@@ -41,11 +41,7 @@ class Mixes extends Component {
     super(props);
     this.state = {
       player: false,
-      currentTrackIndex: -1,
-      playing: false,
-      paused: false,
-      currentTime: 0,
-      duration: 0
+      currentTrackIndex: -1
     };
  }
 
@@ -58,85 +54,15 @@ class Mixes extends Component {
     // }
   }
 
-  timer = () => this.setState({currentTime: this.audioElement.currentTime});
-
   selectTrackNumber = (trackId) => {
-    this.setState({currentTrackIndex:trackId, playing:true, player: true, currentTime: 0}, this.playAudio);
+    this.setState({currentTrackIndex:trackId, player: true});
   }
 
-  loadDuration = () => {
-    // console.log(this.audioElement.duration);
-    this.setState({duration: this.audioElement.duration});
-  }
-
-  playAudio = () => {
-    // console.log(this.audioElement.currentTime);
-    if (this.state.currentTime > 0) {
-      this.audioElement.currentTime = this.state.currentTime;
-    }
-    else {
-      this.audioElement.load();
-    }
-
-    this.audioElement.play();
-  }
-
-  pauseAudio = () => {
-    // clearInterval(this.state.interval);
-    this.setState({ currentTime: this.audioElement.currentTime });
-    this.audioElement.pause();
-  }
-
-  stopAudio = () => {
-    // clearInterval(this.state.interval);
-    this.audioElement.currentTime = 0;
-    this.audioElement.pause();
-  }
-
-  handleSeek = value => {
-    // console.log(value);
-    this.audioElement.currentTime = value;
-  }
-
-  handleClick = (e, {id}, value = 0) => {
-    switch (id) {
-      case 'play':
-        this.setState({currentTrackIndex: this.state.currentTrackIndex, playing:true, paused: false, player: true}, this.playAudio);
-        break;
-      case 'pause':
-        this.setState({playing: false, paused: true}, this.pauseAudio);
-        break;
-      case 'stop':
-        this.setState({playing: false, paused: false, currentTime: 0}, this.stopAudio);
-        break;
-      case "prev":
-        this.setState((state, props) => {
-          let currentIndex = state.currentTrackIndex - 1;
-          if (currentIndex < 0) {
-            return null;
-          } else {
-            return { playing:true, paused: false, currentTrackIndex: currentIndex, currentTime: 0 };
-          }
-        },this.playAudio);
-        break;
-      case "next":
-        this.setState((state, props) => {
-          let currentIndex = state.currentTrackIndex + 1;
-          if (currentIndex > playlist.length - 1) {
-            return null;
-          } else {
-            return { playing:true, paused: false, currentTrackIndex: currentIndex, currentTime: 0 };
-          }
-        },this.playAudio);
-        break;
-      default:
-        break;
-    }
-  }
+  changeIndex = index => this.setState({currentTrackIndex: index});
 
   render() {
-    const { player, currentTrackIndex, playing, currentTime, duration } = this.state;
-    // console.log(this.state.currentTime);
+    const { player, currentTrackIndex } = this.state;
+    // console.log(currentTrackIndex);
     return (
       <Container style={{position: 'relative'}}>
         <List animated divided verticalAlign='middle'>
@@ -154,22 +80,12 @@ class Mixes extends Component {
         <Divider />
         {
           player &&
-          <Fragment>
-            <AudioPlayer
-              currentTime={currentTime}
-              duration={duration}
-              playing={playing}
-              handleClick={this.handleClick}
-              handleSeek={this.handleSeek}
-              current={playlist[currentTrackIndex]}
-            />
-            <audio
-              ref={audio => {this.audioElement = audio}}
-              src={playlist[currentTrackIndex].src}
-              onLoadedMetadata={this.loadDuration}
-              onTimeUpdate={this.timer}
-            />
-          </Fragment>
+          <AudioPlayer
+            changeIndex={this.changeIndex}
+            current={playlist[currentTrackIndex]}
+            currentTrackIndex={currentTrackIndex}
+            length={playlist.length}
+          />
         }
       </Container>
     );
