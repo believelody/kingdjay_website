49e1759b1252f5c84518e56aefe241eee081b42c
believelody <believelody@gmail.com>
2018-10-31 14:45:54 +0100

    test another env variables

--- src/mailgunCred.js
+++ src/mailgunCred.js
@@ -1,3 +1,3 @@
-import mailgun from 'mailgun-js';
+import mailgun from 'mailgun.js';
 
 export default mailgun.client({username: 'api', key: process.env.REACT_APP_MAILGUN_API_KEY || 'your mailgun api key here'});
