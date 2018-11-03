df1c31b9464ac468894714a3704af8ee9cd55170
believelody <believelody@gmail.com>
2018-11-03 21:08:15 +0100

    Test in production: failed 3. Mailgun credentials are the problems

--- misc/mailer.js
+++ misc/mailer.js
@@ -2,9 +2,11 @@ const nodemailer = require('nodemailer');
 const config = require('../config/mailer');
 
 module.exports = nodemailer.createTransport({
-  service: 'Mailgun',
+  host: 'smtp.mailgun.org',
+  prot: 2525,
+  secure: false,
   auth: {
-    user: config.MAILGUN_DOMAIN,
+    user: config.MAILGUN_USER,
     pass: config.MAILGUN_PASS
   },
   tls: {
