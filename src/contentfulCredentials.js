bf5cf5fba9017cc8d432b508069043bf818a2393
believelody <believelody@gmail.com>
2018-11-03 20:11:25 +0100

    Correct mispell for contentful space id in contentful credentials file

--- src/contentfulCredentials.js
+++ src/contentfulCredentials.js
@@ -1,6 +1,6 @@
 import * as contentful from 'contentful';
 
 export default contentful.createClient({
-  space: process.env.REACT_APP_CONTENTFUL_SOACE_ID || 'enter your contentful space id here',
+  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID || 'enter your contentful space id here',
   accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN || 'enter your contentful access token here'
 });
